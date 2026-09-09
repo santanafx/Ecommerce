7e1ee41118c72e211deb87dcc0ae7e66cdbd0868
lucas <santanafx@hotmail.com>
2026-09-09 17:22:01 -0300

    feat(sale): add SaleDto and constructor from DTO

---
+++ Ecommerce/DTOs/SaleDto.cs
@@ -0,0 +1,4 @@
+public class SaleDto
+{
+  public ICollection<Product> ProductsList { get; set; }
+}
--- Ecommerce/Models/Sale.cs
+++ Ecommerce/Models/Sale.cs
@@ -1,9 +1,17 @@
 public class Sale
 {
   public Guid Id { get; private set; }
-  public string Name { get; private set; }
   public DateTime CreatedAt { get; private set; }
+  public ICollection<Product> ProductsList { get; private set; }
   public decimal Total { get; private set; }
   public ICollection<ProductSaleItem> Products { get; private set; }
 
+  private Sale() { }
+
+  public Sale(SaleDto dto)
+  {
+    Id = Guid.NewGuid();
+    ProductsList = dto.ProductsList;
+    CreatedAt = DateTime.UtcNow;
+  }
 }
