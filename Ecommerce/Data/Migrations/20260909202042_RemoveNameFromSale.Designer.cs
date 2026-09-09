f2d0b8bd7dfa2f633b3e2a257546a07bd8a70cab
lucas <santanafx@hotmail.com>
2026-09-09 17:21:23 -0300

    refactor(data): drop Name from Sales, add SaleId

--- Ecommerce/Data/Migrations/20260909202042_RemoveNameFromSale.Designer.cs
+++ Ecommerce/Data/Migrations/20260909202042_RemoveNameFromSale.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Ecommerce.Data.Migrations
 {
     [DbContext(typeof(DbContextEcommerce))]
-    partial class DbContextEcommerceModelSnapshot : ModelSnapshot
+    [Migration("20260909202042_RemoveNameFromSale")]
+    partial class RemoveNameFromSale
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Ecommerce/Data/Migrations/20260909202042_RemoveNameFromSale.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveNameFromSale : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Sales");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "SaleId",
+                table: "Products",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_SaleId",
+                table: "Products",
+                column: "SaleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Sales_SaleId",
+                table: "Products",
+                column: "SaleId",
+                principalTable: "Sales",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Sales_SaleId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_SaleId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "SaleId",
+                table: "Products");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Sales",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
