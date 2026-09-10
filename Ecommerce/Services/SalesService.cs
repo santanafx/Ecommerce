b868e918f3638aa644b03bede78c08590c271722
lucas <santanafx@hotmail.com>
2026-09-10 15:22:56 -0300

    feat(sales): add SalesService for managing sales

---
+++ Ecommerce/Services/SalesService.cs
@@ -0,0 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+
+public class SalesService : ISalesService
+{
+  private readonly DbContextEcommerce _dbContextEcommerce;
+
+  public SalesService(DbContextEcommerce dbContextEcommerce)
+  {
+    _dbContextEcommerce = dbContextEcommerce;
+
+  }
+
+  public async Task<Sale> NewSale(SaleDto saleDto)
+  {
+    var sale = new Sale(saleDto);
+
+    _dbContextEcommerce.Sales.Add(sale);
+    await _dbContextEcommerce.SaveChangesAsync();
+
+    return sale;
+  }
+
+  public async Task<Sale> Sale(Guid id)
+  {
+    return await _dbContextEcommerce.Sales.FindAsync(id);
+  }
+
+  public async Task<ICollection<Sale>> Sales()
+  {
+    return await _dbContextEcommerce.Sales.ToListAsync();
+  }
+
+  public async Task<PagedResponse<Sale>> GetSalesPaginated(PaginationParams paginationParams)
+  {
+    var query = _dbContextEcommerce.Sales.AsQueryable();
+
+    if (!string.IsNullOrEmpty(paginationParams.SearchTerm))
+      query = query.Where(p => p.Id.ToString().Contains(paginationParams.SearchTerm));
+
+    var totalRecords = await query.CountAsync();
+
+    var sales = await query
+      .Skip((paginationParams.PageNumber - 1) * paginationParams.PageSize)
+      .Take(paginationParams.PageSize)
+      .ToListAsync();
+
+    return new PagedResponse<Sale>(sales, paginationParams.PageNumber, paginationParams.PageSize, totalRecords);
+  }
+}
