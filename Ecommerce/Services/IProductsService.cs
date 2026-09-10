fd044abdee6ebf472252547248e6ff2ef2390279
lucas <santanafx@hotmail.com>
2026-09-10 15:23:42 -0300

    feat(ProductsService): add product pagination

--- Ecommerce/Services/IProductsService.cs
+++ Ecommerce/Services/IProductsService.cs
@@ -1,6 +1,7 @@
 public interface IProductsService
 {
   Task<ICollection<Product>> Products();
+  Task<PagedResponse<Product>> GetProductsPaginated(PaginationParams paginationParams);
   Task<Product> AddProduct(ProductDto productDto);
   Task<Product> RemoveProduct(Guid id);
   Task<Product> UpdateProduct(Guid id, ProductDto productDto);
