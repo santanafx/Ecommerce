1f0479eb26a6120e7de1c69579f22eee28174d42
lucas <santanafx@hotmail.com>
2026-09-10 15:28:20 -0300

    feat(SalesService): add search to paginated sales

--- Ecommerce/Services/SalesService.cs
+++ Ecommerce/Services/SalesService.cs
@@ -10,6 +10,24 @@ public class SalesService : ISalesService
 
   }
 
+  public async Task<PagedResponse<Sale>> GetSalesPaginated(PaginationParams paginationParams)
+  {
+    var query = _dbContextEcommerce.Sales.AsQueryable();
+
+    if (!string.IsNullOrEmpty(paginationParams.SearchTerm))
+      query = query.Where(p => p.Id.ToString().Contains(paginationParams.SearchTerm));
+
+    var totalRecords = await query.CountAsync();
+
+    var sales = await query
+      .Skip((paginationParams.PageNumber - 1) * paginationParams.PageSize)
+      .Take(paginationParams.PageSize)
+      .ToListAsync();
+
+    return new PagedResponse<Sale>(sales, paginationParams.PageNumber, paginationParams.PageSize, totalRecords);
+  }
+
+
   public async Task<Sale> NewSale(SaleDto saleDto)
   {
     var sale = new Sale(saleDto);
@@ -28,22 +46,6 @@ public class SalesService : ISalesService
   public async Task<ICollection<Sale>> Sales()
   {
     return await _dbContextEcommerce.Sales.ToListAsync();
-  }
 
-  public async Task<PagedResponse<Sale>> GetSalesPaginated(PaginationParams paginationParams)
-  {
-    var query = _dbContextEcommerce.Sales.AsQueryable();
-
-    if (!string.IsNullOrEmpty(paginationParams.SearchTerm))
-      query = query.Where(p => p.Id.ToString().Contains(paginationParams.SearchTerm));
-
-    var totalRecords = await query.CountAsync();
-
-    var sales = await query
-      .Skip((paginationParams.PageNumber - 1) * paginationParams.PageSize)
-      .Take(paginationParams.PageSize)
-      .ToListAsync();
-
-    return new PagedResponse<Sale>(sales, paginationParams.PageNumber, paginationParams.PageSize, totalRecords);
   }
 }
